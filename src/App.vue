79dfd0f0814f8115875238d7de85d78e34950e0c
squyz <matveevevgenij77@gmail.com>
2023-10-06 21:05:34 +0300

    day1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="display">
+    <input class="value" v-model='visualValue'/>
+    <div class="buttons">
+      <my-button color="darkgray" @clickBtn="clickBtn" value="C">C</my-button>
+      <my-button color="darkgray" @clickBtn="clickBtn" value="+/-">+/-</my-button>
+      <my-button color="darkgray" value="%" @clickBtn="clickBtn">%</my-button>
+      <my-button color="orange" value="/" @clickBtn="clickBtn" ref="action">/</my-button>
+      <my-button color="fairgray" value="7" @clickBtn="clickBtn">7</my-button>
+      <my-button color="fairgray" value="8" @clickBtn="clickBtn">8</my-button>
+      <my-button color="fairgray" value="9" @clickBtn="clickBtn">9</my-button>
+      <my-button color="orange" value="x" @clickBtn="clickBtn" ref="action">x</my-button>
+      <my-button color="fairgray" value="4" @clickBtn="clickBtn">4</my-button>
+      <my-button color="fairgray" value="5" @clickBtn="clickBtn">5</my-button>
+      <my-button color="fairgray" value="6" @clickBtn="clickBtn">6</my-button>
+      <my-button color="orange" value="-" @clickBtn="clickBtn" ref="action">-</my-button>
+      <my-button color="fairgray" value="1" @clickBtn="clickBtn">1</my-button>
+      <my-button color="fairgray" value="2" @clickBtn="clickBtn">2</my-button>
+      <my-button color="fairgray" value="3" @clickBtn="clickBtn">3</my-button>
+      <my-button color="orange" value="+" @clickBtn="clickBtn" ref="action">+</my-button>
+      <my-button color="fairgray" class="btn-long" value="0" @clickBtn="clickBtn">0</my-button>
+      <my-button color="fairgray" value="." @clickBtn="clickBtn" ref="action">.</my-button>
+      <my-button color="fairgray" value="=" @clickBtn="clickBtn">=</my-button>
+  </div>
+  </div>
 </template>
 
+<script>
+import MyButton from './components/MyButton'
+export default {
+  data(){
+    return {
+      visualValue: '',
+      finalValue: 0,
+    }
+  },
+  components: {
+    MyButton
+  },
+  methods: {
+    clickBtn(value){
+      if(value=='+' || value=='-' || value=='/' || value=='x' || value=='.' || value==''){
+        console.log(this.$refs.action.$attrs)
+      }
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  .display{
+    display: flex;
+    flex-direction: column;
+    height: 524px;
+    justify-content: flex-end;
+    width: calc(64px * 4 + 12px * 3);
+    background-color: black;
+    padding: 20px;
+    border-radius: 15px;
+  }
+  .value{
+    text-align: end;
+    color: white;
+    font-size: 48px;
+    border: none;
+    background: none;
+  }
+  .buttons{
+    display: flex;
+    gap: 12px;
+    flex-wrap: wrap;
+    align-items: flex-end;
+    width: calc(64px * 4 + 12px * 3);
+  }
+  .btn-long{
+    flex: 2;
+  }
 </style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button :style="{backgroundColor: color}" @click="btnClick" type="submit">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        color:{
+            default: 'black',
+            type: String
+        }
+    },
+    methods: {
+        btnClick(event){
+            this.$emit('clickBtn', event.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+button{
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    border: none;
+    color: white;
+    font-size: 24px;
+}
+</style>
