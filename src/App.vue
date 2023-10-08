39e8683d98e2ce3f59c46865e8b3406e137d587e
squyz <matveevevgenij77@gmail.com>
2023-10-08 15:40:44 +0300

    day2

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,21 @@
       <my-button color="darkgray" @clickBtn="clickBtn" value="C">C</my-button>
       <my-button color="darkgray" @clickBtn="clickBtn" value="+/-">+/-</my-button>
       <my-button color="darkgray" value="%" @clickBtn="clickBtn">%</my-button>
-      <my-button color="orange" value="/" @clickBtn="clickBtn" ref="action">/</my-button>
+      <my-button color="orange" value="/" @clickBtn="clickBtn" ref="action" :disabled="disabledOn">/</my-button>
       <my-button color="fairgray" value="7" @clickBtn="clickBtn">7</my-button>
       <my-button color="fairgray" value="8" @clickBtn="clickBtn">8</my-button>
       <my-button color="fairgray" value="9" @clickBtn="clickBtn">9</my-button>
-      <my-button color="orange" value="x" @clickBtn="clickBtn" ref="action">x</my-button>
+      <my-button color="orange" value="*" @clickBtn="clickBtn" ref="action" :disabled="disabledOn">x</my-button>
       <my-button color="fairgray" value="4" @clickBtn="clickBtn">4</my-button>
       <my-button color="fairgray" value="5" @clickBtn="clickBtn">5</my-button>
       <my-button color="fairgray" value="6" @clickBtn="clickBtn">6</my-button>
-      <my-button color="orange" value="-" @clickBtn="clickBtn" ref="action">-</my-button>
+      <my-button color="orange" value="-" @clickBtn="clickBtn" ref="action" :disabled="disabledOn">-</my-button>
       <my-button color="fairgray" value="1" @clickBtn="clickBtn">1</my-button>
       <my-button color="fairgray" value="2" @clickBtn="clickBtn">2</my-button>
       <my-button color="fairgray" value="3" @clickBtn="clickBtn">3</my-button>
-      <my-button color="orange" value="+" @clickBtn="clickBtn" ref="action">+</my-button>
+      <my-button color="orange" value="+" @clickBtn="clickBtn" ref="action" :disabled="disabledOn">+</my-button>
       <my-button color="fairgray" class="btn-long" value="0" @clickBtn="clickBtn">0</my-button>
-      <my-button color="fairgray" value="." @clickBtn="clickBtn" ref="action">.</my-button>
+      <my-button color="fairgray" value="." @clickBtn="clickBtn" ref="action" :disabled="disabledOn">.</my-button>
       <my-button color="fairgray" value="=" @clickBtn="clickBtn">=</my-button>
   </div>
   </div>
@@ -32,6 +32,7 @@ export default {
     return {
       visualValue: '',
       finalValue: 0,
+      disabledOn: false
     }
   },
   components: {
@@ -39,8 +40,26 @@ export default {
   },
   methods: {
     clickBtn(value){
-      if(value=='+' || value=='-' || value=='/' || value=='x' || value=='.' || value==''){
-        console.log(this.$refs.action.$attrs)
+      if(value=='+' || value=='-' || value=='/' || value=='*' || value=='.'){
+        if(this.visualValue){
+          console.log('hello!');
+          this.disabledOn = true;
+          this.visualValue += value;
+        }
+      }
+      else if(isFinite(value)){
+        this.disabledOn = false;
+        this.visualValue += value
+      }
+      else if(value == 'C'){
+        this.visualValue = this.visualValue.slice(0, -1);
+        this.disabledOn = false;
+      }
+      else if(value=='%'){
+        this.visualValue += value;
+      }
+      else if(value == '='){
+        this.visualValue = eval(this.visualValue)
       }
     }
   }
@@ -68,6 +87,8 @@ export default {
     font-size: 48px;
     border: none;
     background: none;
+    display: flex;
+    flex-wrap: wrap;
   }
   .buttons{
     display: flex;
